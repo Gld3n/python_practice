3dfcef943380adb25992a01e1af77d36c0ef112b
Gld3n <robertvale301104@gmail.com>
2023-02-12 01:10:43 -0400

    Exploring formating and lists curiosities.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-def main():
-        print("Fuck you.")
+import random
 
-main()
+random_int = random.randint(0, 100)
+
+def curse_random_int():
+        print(f"Fuck you, {random_int}")
+
+curse_random_int()
+
+names_list = ["Ashley", "Robert", "Alejandro", "Jose", "Brigitte"]
+you_are_a_bitch = True
+
+if len(names_list) >= random_int or you_are_a_bitch: # Of course you're
+        names_list.append("Barry")
+
+print(names_list)
+print(names_list[1+1])
+
+liste = [1, 2]
+one, _ = liste
+print(one)
