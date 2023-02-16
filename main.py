b4b0e3fe25b9b5a4819ed14202bdcaaae1c5890f
Gld3n <robertvale301104@gmail.com>
2023-02-16 00:32:33 -0400

    Exceptions

--- main.py
+++ main.py
@@ -94,3 +94,5 @@ my_capy.slay()        # (solution) the function is being called directly  // if 
 Capybara("GG").slay() # also a way to call it without assigning it // same case as above
 
 print("==================================================================")
+
+# Exceptions
