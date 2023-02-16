7a6a5dbdaae2544536a537dc49ca1ffd35b954f1
Gld3n <robertvale301104@gmail.com>
2023-02-16 00:30:25 -0400

    Learned about funcs and classes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+print("==================================================================")
+
 random_int = random.randint(0, 100)
 
 def curse_random_int():
@@ -7,6 +9,7 @@ def curse_random_int():
 
 curse_random_int()
 
+print("==================================================================")
 
 names_list = ["Ashley", "Robert", "Alejandro", "Jose", "Brigitte"]
 you_are_a_bitch = True
@@ -17,11 +20,13 @@ if len(names_list) >= random_int or you_are_a_bitch: # Of course you're
 print(names_list)
 print(names_list[1+1])
 
+print("==================================================================")
 
 liste = [1, 2]
 one, _ = liste
 print(one)
 
+print("==================================================================")
 
 # Commented because it was annoying on runtime
 # while I was trying other things.
@@ -41,6 +46,7 @@ elif len(capyslay) < 10:
 new_capy = {'py', 'go', 'rs'}
 print(new_capy.union(capyslay))
 
+print("==================================================================")
 
 giorgio_dict = {
     "name": "Giorgio",
@@ -57,6 +63,34 @@ for i in giorgio_dict.values():
     else:
         print("- Giorgio is an amazing duck.")
 
+print("==================================================================")
+
 capy_is_god = True
 if capy_is_god:
     print("Capy supremacy")
+
+print("==================================================================")
+
+def print_lots_of_capys(*capys):
+    for capy in capys:
+        print(f"I love {capy}")
+
+print_lots_of_capys("Gort", "Risotto", "Robert", "Chigui")
+
+print("==================================================================")
+
+class Capybara:
+    def __init__(self, name: str):
+        self.name = name
+
+    def slay(self):
+        print(f"{self.name} slays") # it's a better practice to use a return, and so, I can use the print statement as below
+
+my_capy = Capybara("Gort")
+print(my_capy.name)
+print(my_capy.slay()) # prints None afterwards (solved: it was printing a print)
+my_capy.slay()        # (solution) the function is being called directly  // if return is used, this doesn't print anything
+
+Capybara("GG").slay() # also a way to call it without assigning it // same case as above
+
+print("==================================================================")
