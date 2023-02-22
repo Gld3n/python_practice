5c83fc142fe1e475dc61cc5d59c065657f45b883
Gld3n <robertvale301104@gmail.com>
2023-02-22 00:26:55 -0400

    Gave each excercise a separate file and solved prime numbers challenge

--- excercises/fizzbuzz.py
+++ excercises/fizzbuzz.py
@@ -7,7 +7,7 @@ make a program that prints the following:
 - Multuple of both - print FizzBuzz.
 """
 
-def fibu():
+def fibu() -> None:
     for i in range(1,101):
         if i % 3 == 0 and i % 5 == 0:
             print(f"{i} - FizzBuzz!")
@@ -19,58 +19,3 @@ def fibu():
             print(i)
                 
 fibu()
-
-
-"""
-! Anagram challenge
-Define a function that takes two arguments (strings) and 
-returns a boolean.
-- It's not needed to check if both words exist.
-- Two identical words are not an anagram.
-"""
-
-# * Verbose solution
-def is_anagram(word_one, word_two) -> bool:
-    word_one_list = {i for i in word_one}
-    word_two_list = {i for i in word_two}
-    diff = word_one_list.difference(word_two_list)
-    
-    if word_one.lower() == word_two.lower():
-        return False
-    elif not diff:
-        print("Is_anagram")
-        return True
-        
-    return word_one == word_two
-
-is_anagram("amor", "roma")
-
-# * Direct solution
-def is_anagram_2(first_word, second_word) -> bool:
-    if first_word.lower() == second_word.lower():
-        return False
-    
-    return sorted(first_word.lower()) == sorted(second_word.lower())
-
-print(is_anagram_2("pato", "topa"))
-
-
-"""
-! Fibonacci challenge
-Write a program that prints the first 50 numbers of the 
-fibonacci succession starting from 0.
-- Fibonacci sequence is a succession where the
-next number is the sum of the last two numbers.
-- 0 1 1 2 3 5 8 13 21...
-"""
-
-def fib():
-    prev = 0
-    next = 1
-    for i in range(0, 50):
-        fibo = prev + next
-        prev = next
-        next = fibo
-        print(fibo)
-        
-fib()
