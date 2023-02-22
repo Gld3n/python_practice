334618d261fcb14a3bf9a2de19d6c580f76bc390
Gld3n <robertvale301104@gmail.com>
2023-02-21 21:23:26 -0400

    Learning about modules and their implementation.

--- 02_intermediate.py
+++ 02_intermediate.py
@@ -1,1 +1,23 @@
-pass
+from datetime import datetime
+
+now = datetime.now()
+timestamp = datetime.timestamp(now)
+
+date = {
+        "year": now.year,
+        "month": now.month,
+        "day": now.day,
+        "hour": now.hour,
+        "minute": now.minute
+}
+
+def formated_date(date) -> datetime:
+        year = date["year"] 
+        month = datetime.strptime(str(date["month"]), "%m").strftime("%B")
+        day = date["day"]
+        hour = date["hour"]
+        minute = date["minute"]
+        return f"Today is {month} {day}, {year}. It's {hour} hours with {minute} minutes."
+
+f_date = formated_date(date)
+print(f_date)
