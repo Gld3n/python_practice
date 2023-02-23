4fe0036063bcfd837d22d48928655c02de0fc9b7
Gld3n <robertvale301104@gmail.com>
2023-02-22 21:56:44 -0400

    Solved the inverted string excercise.

---
+++ excercises/inverted.py
@@ -0,0 +1,19 @@
+"""
+! Inverted string challenge
+Write a program that prints a reversed given string without
+using any built-in function that does it automatically.
+- 'Hello world' ==> 'dlrow olleH' 
+"""
+
+def reversed_text(text) -> str:
+    
+    if type(text) != str:
+        return "That's not a string I can reverse."
+    
+    reversed_tc = ""
+    for i in text[::-1]:
+        reversed_tc += i
+        
+    return reversed_tc
+
+print(reversed_text("Hello world!"))
