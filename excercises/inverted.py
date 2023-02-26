c9b5a5dd9b922374ee5c581e32358470f7245925
Gld3n <robertvale301104@gmail.com>
2023-02-25 23:36:23 -0400

    Improving my abilities and learning better practices

--- excercises/inverted.py
+++ excercises/inverted.py
@@ -5,6 +5,7 @@ using any built-in function that does it automatically.
 - 'Hello world' ==> 'dlrow olleH' 
 """
 
+#* Old version. More expensive and slower to execute
 def reversed_text(text: str) -> str:
     
     if type(text) != str:
@@ -12,8 +13,22 @@ def reversed_text(text: str) -> str:
     
     reversed_tc: str = ""
     for i in text[::-1]:
-        reversed_tc += i
+        reversed_tc += i #! This will create a whole new string
         
     return reversed_tc
 
 print(reversed_text("Hello world!"))
+
+
+#* Faster and more efficient way to do it
+def new_reversed(text: str) -> str:
+    
+    if type(text) != str:
+        return "That's not a string I can reverse."
+    
+    reversed_tc: list = [i for i in text[::-1]]
+        
+    #* Join method will add each element of the list into a string
+    return "".join(reversed_tc)
+
+print(new_reversed("Hello world!"))
